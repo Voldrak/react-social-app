5a3173b3eea08f202b2188b88b0a9a7313d284c0
MarcoM94 <voldrak94@gmail.com>
2021-12-15 13:01:45 +0100

    feat: nuovi components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header/index";
+import Footer from "./components/Footer/index";
+import Home from "./pages/Home/index.jsx";
 
 function App() {
+  const name = "Feisbrut";
+  const nav= [
+    {link:"/", label: "Home"},
+    {link:"/Friends", label: "Friends"},
+    {link:"/Messages", label: "Messages"},
+];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header name={name} links={nav} />
+      <Home />
+      <Footer />
+      
     </div>
   );
 }
---
+++ src/components/FriendPreview/index.jsx
@@ -0,0 +1,15 @@
+import styles from "./FriendPreview.module.scss"
+
+const FriendPreview = (props) => {
+    const data = props.data || {
+         name: 'Friend', photo:"https://randomuser.me/api/portraits/lego/1.jpg"};
+    return (
+    <div className={styles.friend}>
+        <img src={data.photo} alt={data.name} />
+        <p>{data.name}</p>
+        
+    </div>
+    )
+};
+
+export  { FriendPreview };
